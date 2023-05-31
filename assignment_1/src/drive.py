b750614f4fd0dc479b524587a1d27a4b6042cf3c
Junho Kim <73748884+junho2000@users.noreply.github.com>
2023-05-31 20:54:23 +0900

    :sparkles: Feat: 새로운 기능 추가
    hahaha

--- assignment_1/src/drive.py
+++ assignment_1/src/drive.py
@@ -6,7 +6,8 @@ from std_msgs.msg import Int32MultiArray
 from xycar_msgs.msg import xycar_motor
 
 ultrasonicData = [0.0, 0.0, 0.0, 0.0, 0.0]
-
+# 0 left 1 lf 2 f 3 rf 4 r
+# 0 lf 1 f 2 rf 6 r 7 l
 def callback(msg): 
     global ultrasonicData
     ultrasonicData = msg.data  
@@ -18,8 +19,22 @@ motor_pub = rospy.Publisher('xycar_motor', xycar_motor, queue_size=1)
 xycar_msg = xycar_motor()
 
 while not rospy.is_shutdown():
+
     angle = 0
     speed = 10
     xycar_msg.angle = angle
     xycar_msg.speed = speed
     motor_pub.publish(xycar_msg)
+
+    if ultrasonicData[1] < ultrasonicData[3]:
+        xycar_msg.angle = (ultrasonicData[3] - ultrasonicData[1] - 10)
+        xycar_msg.speed = 60
+        motor_pub.publish(xycar_msg)
+    elif ultrasonicData[3] < ultrasonicData[1]:
+        xycar_msg.angle = (ultrasonicData[3] - ultrasonicData[1] - 10)
+        xycar_msg.speed = 60
+        motor_pub.publish(xycar_msg)
+    else:
+        xycar_msg.angle = 0
+        xycar_msg.speed = 120
+        motor_pub.publish(xycar_msg)
