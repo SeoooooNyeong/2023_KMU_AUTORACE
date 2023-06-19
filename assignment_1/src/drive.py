6f918389b69526d18f883c5045a83c65cd6734d9
Lee Sehyeon <84698896+sehyeon518@users.noreply.github.com>
2023-06-19 16:26:51 +0900

    :bulb: docs: 주석 추가

--- assignment_1/src/drive.py
+++ assignment_1/src/drive.py
@@ -18,25 +18,42 @@ motor_pub = rospy.Publisher('xycar_motor', xycar_motor, queue_size=1)
 
 xycar_msg = xycar_motor()
 
+# while loop 실행 횟수를 세는 변수
 count = 0
 
 while not rospy.is_shutdown():
 
-    
+    #=========================================
+    # 첫번째 실행인 경우
+    # angle=0, speed=50으로 설정한다
+    #=========================================
     if count == 0:
         xycar_msg.angle = 0
-        xycar_msg.speed = 10
+        xycar_msg.speed = 50
         count += 1
-     
-    elif ultrasonicData[1] < ultrasonicData[3]: # left front < right front
+    #=========================================
+    # 왼쪽으로 치우친 경우 (왼쪽 앞 센서 < 오른쪽 앞 센서)
+    # 오른쪽으로 30도 회전한다
+    #=========================================
+    elif ultrasonicData[1] < ultrasonicData[3]:
         xycar_msg.angle = 30
         xycar_msg.speed = 50
-    elif ultrasonicData[3] < ultrasonicData[1]: # right front < left front
+    #=========================================
+    # 오른쪽으로 치우친 경우 <왼쪽 앞 센서 > 오른쪽 앞 센서)
+    # 왼쪽으로 30도 회전한다
+    #=========================================
+    elif ultrasonicData[3] < ultrasonicData[1]:
         xycar_msg.angle = -30
         xycar_msg.speed = 50
+    #=========================================
+    # 왼쪽 앞 센서와 오른쪽 앞 센서의 차이가 없는 경우
+    # 정면으로 주행한다
+    #=========================================
     else:
         xycar_msg.angle = 0
         xycar_msg.speed = 50
+        
+    #=========================================
+    # 변경된 각도와 속도 publish
+    #=========================================
     motor_pub.publish(xycar_msg)
-
-    print('speed:', xycar_msg.speed)
