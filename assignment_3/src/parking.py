62a3de17e4f46825e4bb045fa064e9051f449181
Lee Sehyeon <84698896+sehyeon518@users.noreply.github.com>
2023-06-20 14:54:42 +0900

    :art: style: 변수 선언 위치 변경

--- assignment_3/src/parking.py
+++ assignment_3/src/parking.py
@@ -10,9 +10,6 @@ import math
 import rospy
 from xycar_msgs.msg import xycar_motor
 
-# parameter
-MAX_T = 100.0  # maximum time to the goal [s]
-MIN_T = 10.0  # minimum time to the goal[s]
 #=============================================
 # 모터 토픽을 발행할 것임을 선언
 #============================================= 
@@ -26,131 +23,27 @@ rx, ry = [], []
 i = 0 # 글로벌 i 선언
 iMax = 0
 
-k = 0.5  # control gain
+k  = 0.5   # control gain
 Kp = 1.0  # speed proportional gain
 dt = 0.1  # [s] time difference
-L = 2.9  # [m] Wheel base of vehicle
+L  = 2.9   # [m] Wheel base of vehicle
 max_steer = np.radians(30.0)  # [rad] max steering angle
 
 show_animation = True
 
+#=============================================
+# 프로그램에서 사용할 상수 선언부
+#=============================================
+AR = (1142, 62) # AR 태그의 위치
+P_ENTRY = (1036, 162) # 주차라인 진입 시점의 좌표
+P_END = (1129, 69) # 주차라인 끝의 좌표
 
-class State(object):
-    """
-    Class representing the state of a vehicle.
+MAX_T = 100.0  # maximum time to the goal[s]
+MIN_T = 10.0   # minimum time to the goal[s]
 
-    :param x: (float) x-coordinate
-    :param y: (float) y-coordinate
-    :param yaw: (float) yaw angle
-    :param v: (float) speed
-    """
-
-    def __init__(self, x=0.0, y=0.0, yaw=0.0, v=0.0):
-        """Instantiate the object."""
-        super(State, self).__init__()
-        self.x = x
-        self.y = y
-        self.yaw = yaw
-        self.v = v
-
-    def update(self, acceleration, delta):
-        """
-        Update the state of the vehicle.
-
-        Stanley Control uses bicycle model.
-
-        :param acceleration: (float) Acceleration
-        :param delta: (float) Steering
-        """
-        delta = np.clip(delta, -max_steer, max_steer)
-
-        self.x += self.v * np.cos(self.yaw) * dt
-        self.y += self.v * np.sin(self.yaw) * dt
-        self.yaw += self.v / L * np.tan(delta) * dt
-        self.yaw = normalize_angle(self.yaw)
-        self.v += acceleration * dt
-
-
-def pid_control(target, current):
-    """
-    Proportional control for the speed.
-
-    :param target: (float)
-    :param current: (float)
-    :return: (float)
-    """
-    return Kp * (target - current)
-
-
-def stanley_control(state, cx, cy, cyaw, last_target_idx):
-    """
-    Stanley steering control.
-
-    :param state: (State object)
-    :param cx: ([float])
-    :param cy: ([float])
-    :param cyaw: ([float])
-    :param last_target_idx: (int)
-    :return: (float, int)
-    """
-    current_target_idx, error_front_axle = calc_target_index(state, cx, cy)
-
-    if last_target_idx >= current_target_idx:
-        current_target_idx = last_target_idx
-
-    # theta_e corrects the heading error
-    theta_e = normalize_angle(cyaw[current_target_idx] - state.yaw)
-    # theta_d corrects the cross track error
-    theta_d = np.arctan2(k * error_front_axle, state.v)
-    # Steering control
-    delta = theta_e + theta_d
-
-    return delta, current_target_idx
-
-
-def normalize_angle(angle):
-    """
-    Normalize an angle to [-pi, pi].
-
-    :param angle: (float)
-    :return: (float) Angle in radian in [-pi, pi]
-    """
-    while angle > np.pi:
-        angle -= 2.0 * np.pi
-
-    while angle < -np.pi:
-        angle += 2.0 * np.pi
-
-    return angle
-
-
-def calc_target_index(state, cx, cy):
-    """
-    Compute index in the trajectory list of the target.
-
-    :param state: (State object)
-    :param cx: [float]
-    :param cy: [float]
-    :return: (int, float)
-    """
-    # Calc front axle position
-    fx = state.x + L * np.cos(state.yaw)
-    fy = state.y + L * np.sin(state.yaw)
-
-    # Search nearest point index
-    dx = [fx - icx for icx in cx]
-    dy = [fy - icy for icy in cy]
-    d = np.hypot(dx, dy)
-    target_idx = np.argmin(d)
-
-    # Project RMS error onto front axle vector
-    front_axle_vec = [-np.cos(state.yaw + np.pi / 2),
-                      -np.sin(state.yaw + np.pi / 2)]
-    error_front_axle = np.dot([dx[target_idx], dy[target_idx]], front_axle_vec)
-
-    return target_idx, error_front_axle
-
-
+#=============================================
+# 
+#=============================================
 class QuinticPolynomial:
     #xqp = QuinticPolynomial(sx, vxs, axs, gx, vxg, axg, T) #도착지 sx, vxs, axs / 목적지 gx, vxg , axg, T
     def __init__(self, xs, vxs, axs, xe, vxe, axe, time):
@@ -194,6 +87,9 @@ class QuinticPolynomial:
 
         return xt
     
+#=============================================
+
+#=============================================
 def quintic_polynomials_planner(sx, sy, syaw, sv, sa, gx, gy, gyaw, gv, ga, max_accel, max_jerk, dt):
     """
     quintic polynomial planner
@@ -274,14 +170,6 @@ def quintic_polynomials_planner(sx, sy, syaw, sv, sa, gx, gy, gyaw, gv, ga, max_
 
     return time, rx, ry, ryaw, rv, ra, rj
 
-
-#=============================================
-# 프로그램에서 사용할 상수 선언부
-#=============================================
-AR = (1142, 62) # AR 태그의 위치
-P_ENTRY = (1036, 162) # 주차라인 진입 시점의 좌표
-P_END = (1129, 69) # 주차라인 끝의 좌표
-
 #=============================================
 # 모터 토픽을 발행하는 함수
 # 입력으로 받은 angle과 speed 값을
@@ -308,13 +196,13 @@ def planning(px, py, ssyaw, max_acceleration, dtt):
     sy = py  # start y position [m]
     syaw = ssyaw  # start yaw angle [rad]
     sv = 10.0  # start speed [m/s]
-    sa = 2.0  # start accel [m/ss]
+    sa = 2.0   # start accel [m/ss]
     
     gx = P_ENTRY[0]  # goal x position [m]
     gy = P_ENTRY[1]  # goal y position [m]
     gyaw = np.deg2rad(-45)  # goal yaw angle [rad]
     gv = 10.0  # goal speed [m/s]
-    ga = 2.0  # goal accel [m/ss]
+    ga = 2.0   # goal accel [m/ss]
     
     max_accel = max_acceleration  # max accel [m/ss]
     max_jerk = 10  # max jerk [m/sss]
